71d8ecdb7b0d521bb92887705a77bfa94d59aa07
Miguel Rodriguez <miguel.a.rodriguez.r@gmail.com>
2020-07-04 16:23:57 -0500

    hotfix

--- src/components/recipe/Recipe.jsx
+++ src/components/recipe/Recipe.jsx
@@ -17,7 +17,7 @@ function getModalStyle() {
 const useStyles = makeStyles((theme) => ({
   paper: {
     position: "absolute",
-    width: 800,
+    width: 400,
     backgroundColor: theme.palette.background.paper,
     boxShadow: theme.shadows[5],
     padding: theme.spacing(2, 4, 3),
